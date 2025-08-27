e05df77cae2c8ff81632e921857244cbd7081794
facundo.do.nacimiento <facudonac@gmail.com>
2025-08-27 00:31:10 -0300

    Agrego categoria, constructor, setter y getter

---
+++ src/main/java/usuario/Categoria.java
@@ -0,0 +1,17 @@
+public class Categoria {
+	private String nombre;
+
+	public Categoria(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
+
