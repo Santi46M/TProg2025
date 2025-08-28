fb7404f45cf445dfa6e8f98139da82bc21936f33
Santiago Mendoza Balestena <santiago.mendoza@fing.edu.uy>
2025-08-28 03:29:26 -0300

    fabrica

---
+++ Tarea1/src/logica/fabrica.java
@@ -0,0 +1,33 @@
+package logica;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import excepciones.CategoriaSinSeleccionarException;
+import excepciones.EdicionYaExisteException;
+import excepciones.EventoYaExisteException;
+import excepciones.InstitucionYaExisteException;
+import excepciones.UsuarioYaExisteException;
+import excepciones.FechasCruzadasException;
+import excepciones.ValorPatrocinioExcedidoException;
+
+public class fabrica {
+	private static fabrica instancia;
+	public static fabrica getInstancia() {
+		if (instancia == null) {
+			instancia = new fabrica();
+		}
+		return instancia;
+	}
+	
+
+    public static iControladorUsuario crearControladorUsuario() {
+        return new controladorUsuario(); 
+    }
+
+    
+    public static iControladorEvento crearControladorEvento() {
+        return new controladorEvento();
+    }
+}
