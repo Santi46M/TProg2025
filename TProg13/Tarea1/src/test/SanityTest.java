6f310861a9fc4d626d5745142e3d9a84b6a8876a
Santiago Mendoza Balestena <santiago.mendoza@fing.edu.uy>
2025-10-12 16:44:25 -0300

    tests

--- TProg13/Tarea1/src/test/SanityTest.java
+++ TProg13/Tarea1/src/test/SanityTest.java
@@ -1,5 +1,5 @@
 package test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class SanityTest {
