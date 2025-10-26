6ad9249d3be0d9eef78fcb76d07f98b1c173a56f
Santiago Mendoza Balestena <santiago.mendoza@fing.edu.uy>
2025-10-26 20:54:20 -0300

    xD

--- TProg13/Tarea3/ClienteWeb/src/main/java/publicadores/DtEvento.java
+++ TProg13/Tarea3/ClienteWeb/src/main/java/publicadores/DtEvento.java
@@ -153,7 +153,7 @@ public class DtEvento {
      *     {@link LocalDate }
      *     
      */
-    public publicadores.LocalDate getFecha() {
+    public LocalDate getFecha() {
         return fecha;
     }
 
