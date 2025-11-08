42454c3084844ff0132b0b586f160e255113da19
nicorenault <nicolasrenaultcasa@gmail.com>
2025-11-07 22:41:53 -0300

    config loader

--- TProg13/Tarea3/ServidorCentral/src/util/ConfigLoader.java
+++ TProg13/Tarea3/ServidorCentral/src/util/ConfigLoader.java
@@ -1,21 +1,31 @@
 package util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigLoader {
-    private static Properties props = new Properties();
+    private static final Properties props = new Properties();
     private static boolean loaded = false;
+
+    private static String getConfigPath() {
+        String userHome = System.getProperty("user.home");
+        String fileSeparator = System.getProperty("file.separator");
+        return userHome + fileSeparator + ".eventosUy" + fileSeparator + "config.properties";
+    }
 
     public static void load() {
         if (loaded) return;
-        String configPath = System.getProperty("user.home") + "\\.eventosUy\\config.properties";
-        try (FileInputStream fis = new FileInputStream(configPath)) {
+
+        String configPath = getConfigPath();
+        File configFile = new File(configPath);
+
+        try (FileInputStream fis = new FileInputStream(configFile)) {
             props.load(fis);
             loaded = true;
         } catch (IOException e) {
-            // Puedes loguear o manejar el error aquí
+            
         }
     }
 
